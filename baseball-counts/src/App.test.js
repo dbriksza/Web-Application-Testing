ea67dbdba67bd8453773f44e75fc14b2e9ec67b5
dbriksza <dbriksza@gmail.com>
2019-10-03 17:26:33 -0400

    MVP

--- baseball-counts/src/App.test.js
+++ baseball-counts/src/App.test.js
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { render } from "@testing-library/react";
 import App from "./App";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+it("Has all the proper displays", () => {
+  const container = render(<App />);
+  container.getByText(/balls/i);
+  container.getByText(/strikes/i);
+});
+it("Has all the proper buttons", () => {
+  const container = render(<App />);
+  container.getByText(/ball!/i);
+  container.getByText(/strike!/i);
+  container.getByText(/foul!/i);
+  container.getByText(/hit!/i);
 });
